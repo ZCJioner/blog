3a859fac069f78c0d519e58fe9371d637b836b14
ZCJioner <changjiang0319@163.com>
2016-01-14 22:57:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<table height="100%" width="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>
     <td bordercolor="0"><table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr>
